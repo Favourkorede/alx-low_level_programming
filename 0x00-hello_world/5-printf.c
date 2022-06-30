deead595975f0bb3a9cb10cbc4ae1605f2a93911
Favourkorede <ayenifavour203@gmail.com>
2022-06-30 15:13:25 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main -Entry point
+ * Description: Using printf to print words
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
