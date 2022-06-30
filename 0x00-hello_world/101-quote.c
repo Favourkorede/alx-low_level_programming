1f5c210d02a773c2fa2009dff30973128486b00d
Favourkorede <ayenifavour203@gmail.com>
2022-06-30 15:39:17 -0700

    UNIX System

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ * Description: 'A new project abot using puts with special symbols'
+ * Return: Always 1 (success)
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
