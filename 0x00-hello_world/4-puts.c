96b026f01ca91874ba7d51c3eefeac71c0f9144e
Favourkorede <ayenifavour203@gmail.com>
2022-06-30 14:09:19 -0700

    Hello ,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: The programmer's life quote
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char str1[52];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	puts(str1);
+	return (0);
+}
