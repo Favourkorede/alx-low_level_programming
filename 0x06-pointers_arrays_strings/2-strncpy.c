10a407a16d60c839c163a99bb949758c95b170ce
Favourkorede <ayenifavour203@gmail.com>
2022-07-14 13:07:01 -0700

    Strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * _strncpy - Concatenates one string to another
- * @dest: This is the string to be copied to
- * @src: This is the string to be copied
- * @n: number of bytes to be copied
- *
- * Return: The destination string
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
-
-	for (; i < n; i++)
-	{
+	for ( ; i < n; i++)
 		dest[i] = '\0';
-	}
 
 	return (dest);
 }
